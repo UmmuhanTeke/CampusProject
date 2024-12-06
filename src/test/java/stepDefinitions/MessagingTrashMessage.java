886fb11d21c3761d605c9835383da897fb884148
SefaKahramannn <sefakahraman0001@gmail.com>
2024-12-07 00:43:00 +0300

    MessagingTrash.feature and MessagingTrashMessage completed

--- src/test/java/stepDefinitions/MessagingTrashMessage.java
+++ src/test/java/stepDefinitions/MessagingTrashMessage.java
@@ -1,43 +1,48 @@
 package stepDefinitions;
 
 import io.cucumber.java.en.*;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.testng.Assert;
 import pages.DialogContent;
-import pages.TopNav;
 
 public class MessagingTrashMessage {
 
     DialogContent dialogContentElement=new DialogContent();
-    TopNav topNavElement=new TopNav();
 
     @And("The user clicks on the trash button")
     public void theUserClicksOnTheTrashButton() {
-    }
-
-    @When("The user views the list of deleted messages")
-    public void theUserViewsTheListOfDeletedMessages() {
+        dialogContentElement.myClick(dialogContentElement.trashBoxBtn);
     }
 
     @Then("The user should display Delete and Restore options for each message")
     public void theUserShouldDisplayDeleteAndRestoreOptionsForEachMessage() {
+        dialogContentElement.wait.until(ExpectedConditions.urlContains("trash"));
+        dialogContentElement.deleteIcon.isDisplayed();
+        dialogContentElement.restoreIcon.isDisplayed();
     }
 
     @When("The user clicks the Restore button for a message")
     public void theUserClicksTheRestoreButtonForAMessage() {
+        dialogContentElement.myClick(dialogContentElement.restoreIcon);
     }
 
     @Then("The message should be successfully restored with a Success message")
     public void theMessageShouldBeSuccessfullyRestoredWithAMessage() {
+        dialogContentElement.verifyContainsText(dialogContentElement.msgControl,"Message successfully");
     }
 
     @When("The user clicks the Delete button for a message")
     public void theUserClicksTheDeleteButtonForAMessage() {
+        dialogContentElement.myClick(dialogContentElement.deleteIcon);
     }
 
     @And("confirms the action in the confirmation popup")
     public void confirmsTheActionInTheConfirmationPopup() {
+        dialogContentElement.myClick(dialogContentElement.deleteBtn);
     }
 
     @Then("The message should be permanently deleted with a Success message")
     public void theMessageShouldBePermanentlyDeletedWithAMessage() {
+        Assert.assertTrue(dialogContentElement.msgControl.getText().contains("Message successfully"));
     }
 }
