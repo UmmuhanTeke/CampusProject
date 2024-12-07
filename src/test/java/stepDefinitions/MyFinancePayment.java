d49339baf542cbfec91dfac80876b76bd3f1ab37
UmmuhanTeke <ummuhantekeee@gmail.com>
2024-12-08 00:50:24 +0300

    My Finance step definitions class has been created.

---
+++ src/test/java/stepDefinitions/MyFinancePayment.java
@@ -0,0 +1,46 @@
+package stepDefinitions;
+
+import io.cucumber.java.en.*;
+
+public class MyFinancePayment {
+
+    @Given("The user has been redirected to the payment page")
+    public void theUserHasBeenRedirectedToThePaymentPage() {
+    }
+
+    @Then("The user views the Online Payment and Fee Balance Detail buttons")
+    public void theUserViewsTheOnlinePaymentAndFeeBalanceDetailButtons() {
+    }
+
+    @And("The user clicks the Stripe button")
+    public void theUserClicksTheStripeButton() {
+    }
+
+    @Then("The user views the payment options")
+    public void theUserViewsThePaymentOptions() {
+    }
+
+    @When("The user clicks the Pay option and views the card information field")
+    public void theUserClicksThePayOptionAndViewsTheCardInformationField() {
+    }
+
+    @And("The user enters card details and completes the payment")
+    public void theUserEntersCardDetailsAndCompletesThePayment() {
+    }
+
+    @Then("The user views a message that the payment was successfully completed")
+    public void theUserViewsAMessageThatThePaymentWasSuccessfullyCompleted() {
+    }
+
+    @Then("The user verifies that the payment has been completed")
+    public void theUserVerifiesThatThePaymentHasBeenCompleted() {
+    }
+
+    @When("The user clicks the New Message button in the top right corner of the toolbar")
+    public void theUserClicksTheNewMessageButtonInTheTopRightCornerOfTheToolbar() {
+    }
+
+    @Then("The user views the received message in the area")
+    public void theUserViewsTheReceivedMessageInTheArea() {
+    }
+}
