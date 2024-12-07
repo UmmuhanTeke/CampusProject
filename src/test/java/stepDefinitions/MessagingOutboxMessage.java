46c8d9a8d7e69d80dc8938c8ea9c2cb1c54bdc16
kayyamervee <kayaamervee01@gmail.com>
2024-12-08 01:21:42 +0300

    login updated

--- src/test/java/stepDefinitions/MessagingOutboxMessage.java
+++ src/test/java/stepDefinitions/MessagingOutboxMessage.java
@@ -2,10 +2,23 @@ package stepDefinitions;
 
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import pages.DialogContent;
 
 public class MessagingOutboxMessage {
+    DialogContent dialogContentElement = new DialogContent();
+
     @When("The user clicks the Yes button in the confirm window by clicking on the Move to Trash icon")
     public void theUserClicksTheYesButtonInTheConfirmWindowByClickingOnTheMoveToTrashIcon() {
+        dialogContentElement.myClick(dialogContentElement.moveToTrashIcon.getFirst());
+
+        dialogContentElement.Wait(3);
+        dialogContentElement.wait.until(ExpectedConditions.visibilityOf(dialogContentElement.confirmTitleText));
+        dialogContentElement.verifyContainsText(dialogContentElement.confirmTitleText,"Confirm");
+
+        dialogContentElement.Wait(3);
+        dialogContentElement.jsClick(dialogContentElement.yesButton);
+
     }
 
     @Then("The user verifies the confirmation message")
