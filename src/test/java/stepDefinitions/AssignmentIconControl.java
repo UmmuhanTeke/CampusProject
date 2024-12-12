7a93499294ebe9ce9dce25bf07b9ddb181810193
kayyamervee <kayaamervee01@gmail.com>
2024-12-13 02:56:32 +0300

    AssignmentsIconControl.feature has been updated

--- src/test/java/stepDefinitions/AssignmentIconControl.java
+++ src/test/java/stepDefinitions/AssignmentIconControl.java
@@ -3,7 +3,6 @@ package stepDefinitions;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 import pages.DialogContent;
 
 public class AssignmentIconControl {
