2ae28c92074ed3ed61cf7f8289b32821dbf6eebb
UmmuhanTeke <ummuhantekeee@gmail.com>
2024-12-14 15:30:23 +0300

    Calendar Video Recording class edited.

--- src/test/java/stepDefinitions/CalendarVideoRecording.java
+++ src/test/java/stepDefinitions/CalendarVideoRecording.java
@@ -15,17 +15,11 @@ public class CalendarVideoRecording {
 
     @When("The user clicks on a completed \\(E) course")
     public void theUserClicksOnACompletedECourse() {
-        try {
-            List<WebElement> endedLessons = dialogContentElement.endedLessonBtn;
-
-            if (!endedLessons.isEmpty()) {
-                int randomIndex = random.nextInt(endedLessons.size());
-                dialogContentElement.myClick(endedLessons.get(randomIndex));
-                dialogContentElement.verifyContainsText(dialogContentElement.popUpVerify, "Course Meeting");
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        List<WebElement> endedLessons = dialogContentElement.endedLessonBtn;
+        int randomIndex = random.nextInt(endedLessons.size());
+        dialogContentElement.myClick(endedLessons.get(randomIndex));
+        dialogContentElement.verifyContainsText(dialogContentElement.popUpVerify, "Information");
+        System.out.println(dialogContentElement.popUpVerify.getText());
     }
 
     @Then("The user views the Recording button to access the course recording and clicks on it")
@@ -40,7 +34,8 @@ public class CalendarVideoRecording {
 
     @And("The user accesses the course video")
     public void theUserAccessesTheCourseVideo() {
-
+        dialogContentElement.wait.until(ExpectedConditions.visibilityOf(dialogContentElement.videoPopUpVerify));
+        dialogContentElement.verifyContainsText(dialogContentElement.videoPopUpVerify, "11A");
     }
 
     @Then("The user views the Play button in the course video and clicks on it")
