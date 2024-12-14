b3ec5aabb2c79921e7a56f559e2f1504c3d55e7a
kayyamervee <kayaamervee01@gmail.com>
2024-12-14 13:43:45 +0300

    CalenderPopUps has been created

---
+++ src/test/java/stepDefinitions/CalenderPopUps.java
@@ -0,0 +1,23 @@
+package stepDefinitions;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class CalenderPopUps {
+    @When("The user views Course Status \\(P\\/S\\/E), Course Name, and Teacher's Name of Weekly Course Plan page")
+    public void theUserViewsCourseStatusPSECourseNameAndTeacherSNameOfWeeklyCoursePlanPage() {
+    }
+
+    @Then("The user verifies the all functions of the a opened pop-up window")
+    public void theUserVerifiesTheAllFunctionsOfTheAOpenedPopUpWindow() {
+    }
+
+    @And("The user clicks on the Course Meeting that has not yet started")
+    public void theUserClicksOnTheCourseMeetingThatHasNotYetStarted() {
+    }
+
+    @Then("The user verifies the Teacher's name, the Date and Time of the lesson, the warning message")
+    public void theUserVerifiesTheTeacherSNameTheDateAndTimeOfTheLessonTheWarningMessage() {
+    }
+}
