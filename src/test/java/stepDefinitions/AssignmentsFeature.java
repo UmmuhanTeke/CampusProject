46f3a8839e5d3aab3767024aab958cbe807a5b63
UmmuhanTeke <ummuhantekeee@gmail.com>
2024-12-15 00:10:14 +0300

    The Assignments class and dialog content added made.

--- src/test/java/stepDefinitions/AssignmentsFeature.java
+++ src/test/java/stepDefinitions/AssignmentsFeature.java
@@ -1,21 +1,32 @@
 package stepDefinitions;
 
 import io.cucumber.java.en.*;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.testng.Assert;
 import pages.DialogContent;
+import utilities.ConfigReader;
+import utilities.GWD;
 
 public class AssignmentsFeature {
     DialogContent dialogContentElement = new DialogContent();
 
     @Given("The user is on the Assignments page")
     public void theUserIsOnTheAssignmentsPage() {
+        dialogContentElement.wait.until(ExpectedConditions.urlToBe(ConfigReader.getProperty("assignmentURL")));
+        Assert.assertTrue(GWD.getDriver().getCurrentUrl().equals(ConfigReader.getProperty("assignmentURL")));
+        dialogContentElement.wait.until(ExpectedConditions.visibilityOf(dialogContentElement.overdueText));
+        dialogContentElement.verifyContainsText(dialogContentElement.overdueText, "Overdue");
     }
 
     @When("The user clicks the search button on the default Assignments page")
     public void theUserClicksTheSearchButtonOnTheDefaultAssignmentsPage() {
+        dialogContentElement.wait.until(ExpectedConditions.visibilityOf(dialogContentElement.searchButton));
+        dialogContentElement.myClick(dialogContentElement.searchButton);
     }
 
     @Then("The user views the list of all assigned tasks")
     public void theUserViewsTheListOfAllAssignedTasks() {
+
     }
 
     @And("The user filters the search \\(Course,Status,Semester)")
